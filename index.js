4b5d511c3bbbacfa8df880af643fa3eb06e80ea0
dfowles1C <dfowles@1concier.com>
2026-04-16 10:50:28 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/monday', async (req, res) => {
+  try {
+    const response = await fetch('https://api.monday.com/v2', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': req.headers['authorization'],
+        'API-Version': '2024-01'
+      },
+      body: JSON.stringify(req.body)
+    });
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(process.env.PORT || 3000);
